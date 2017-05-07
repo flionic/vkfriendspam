2915fcab3d403bc2d4eadff65025f0a4e13ad92a
Bionic Leha <bionic.mods@gmail.com>
2017-05-07 21:06:20 +0300

    New version for installer

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 title [Install - VK Add Friends Bot]
 pip install -r requirements.txt
-python -c "open('settings.cfg', 'w')"
+python -c "cfg = open('settings.cfg', 'w'); cfg.write(input('Enter Target Link:\n')); cfg.close()"
 echo.
 pause
