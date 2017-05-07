d9e3cc9bf22aa20a8da43ce69c085b0f9adb2788
Bionic Leha <bionic.mods@gmail.com>
2017-05-07 20:45:36 +0300

    Release get_settings

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-try:
-    settings_conf = open('text.txt', 'r')
-except FileNotFoundError:
-    print('File not found')
+main_token = ''
+vk_api = 'https://api.vk.com/method/'
+vk_cfg = '&access_token=' + main_token + '&v=5.64'
+settings_conf = {}
+target_ulist = {}
+
+
+def get_settings():
+    global settings_conf
+    try:
+        file_cfg = open('settings.cfg', 'r')
+        settings_conf = [line.strip() for line in file_cfg]
+        print(settings_conf)
+    except FileNotFoundError:
+        print('File not found, creating it')
+        open('settings.cfg', 'w')
+    except Exception as excp:
+        print(excp)
+get_settings()
--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 @echo off
 title [Install - VK Add Friends Bot]
 pip install -r requirements.txt
-python -c 'open('settings.cfg', 'w')'
+python -c "open('settings.cfg', 'w')"
+echo.
 pause
