d666e543ec31c661232140b9ca893fcc473792a3
Bionic Leha <bionic.mods@gmail.com>
2017-05-08 09:58:49 +0300

    Creating data.db

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 title [Install - VK Add Friends Bot]
 pip install -r requirements.txt
-python -c "cfg = open('settings.cfg', 'w'); cfg.write(input('Enter Target Link:\n')); cfg.close()"
+python -c "cfg = open('settings.cfg', 'w'); cfg.write(input('Enter Target Link:\n')); cfg.close(); import sqlite3; data = sqlite3.connect('data.db'); c = data.cursor();c.execute('CREATE TABLE requests (token TEXT, destination TEXT, id TEXT)');data.commit;data.close()"
 echo.
 pause
